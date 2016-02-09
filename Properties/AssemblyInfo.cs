9b47eb9960a1eea73fcced2c5808d80106cc2df4
Kārlis Gaņģis <karlis@zoomcharts.com>
2016-02-09 17:23:46 +0200

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZoomCharts.ProxyEcho")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dcb924ae-357b-424f-9354-3d7e2f6bbf33")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProxyHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web;
+
+namespace ZoomCharts.ProxyEcho
+{
+    public class ProxyHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            // data uri format:
+            // data:[<media type>][;charset=<character set>][;base64],<data>
+            var data = context.Request["data"];
+            
+            var i = data.IndexOf(',', 5);
+            var j = data.IndexOf(';', 5, i - 5);
+            if (j == -1) j = i;
+
+            if (!data.StartsWith("data:") || i == -1)
+            {
+                context.Response.StatusCode = 400;
+                context.Response.StatusDescription = "Bad request";
+                context.Response.Write("The request did not include a valid 'data' parameter.");
+            }
+
+            var type = data.Substring(5, j - 5);
+
+            var mime = type.Length > 0 ? type : "application/octet-stream";
+            var name = context.Request["name"] ?? "export";
+
+            context.Response.AppendHeader("Content-Type", mime);
+            context.Response.AppendHeader("Content-Disposition", "attachment; filename*=UTF-8''" + Uri.EscapeDataString(name));
+            context.Response.AppendHeader("Cache-Control", "private, must-revalidate, max-age=0");
+
+            var base64 = i > 13 && data.Substring(i - 7, 7) == ";base64";
+
+            if (base64)
+            {
+                var buffer = Convert.FromBase64String(data.Substring(i + 1));
+                context.Response.BinaryWrite(buffer);
+            }
+            else
+            {
+                var decoded = Uri.UnescapeDataString(data.Substring(i + 1));
+                context.Response.Write(decoded);
+            }
+        }
+    }
+}
